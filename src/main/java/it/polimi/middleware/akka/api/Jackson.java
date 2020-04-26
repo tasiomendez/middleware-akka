05ae8e492e93e8b7ca380c2e0ffb8db08f4d2de8
Tasio Mendez <tm.tasio@gmail.com>
2020-04-26 21:23:59 +0200

    Documenting functions

--- src/main/java/it/polimi/middleware/akka/api/Jackson.java
+++ src/main/java/it/polimi/middleware/akka/api/Jackson.java
@@ -1,6 +1,7 @@
 package it.polimi.middleware.akka.api;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import akka.http.javadsl.marshalling.Marshaller;
@@ -9,11 +10,25 @@ import akka.http.javadsl.model.RequestEntity;
 
 public class Jackson {
 
+	/**
+	 * Returns a marshaller using a default mapper in order to convert to a 
+	 * JSON object.
+	 * 
+	 * @return marshaller
+	 */
 	public static <T> Marshaller<T, RequestEntity> marshaller() {
-		final ObjectMapper defaultObjectMapper = new ObjectMapper();
+		final ObjectMapper defaultObjectMapper = new ObjectMapper()
+				.enable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY);
 		return marshaller(defaultObjectMapper);
 	}
 
+	/**
+	 * Returns a marshaller using the mapper provided in order to convert to a 
+	 * JSON object.
+	 * 
+	 * @param mapper
+	 * @return marshaller
+	 */
 	public static <T> Marshaller<T, RequestEntity> marshaller(ObjectMapper mapper) {
 		return Marshaller.wrapEntity(
 				u -> toJSON(mapper, u),
@@ -22,6 +37,13 @@ public class Jackson {
 				);
 	}
 
+	/**
+	 * Convert a Java Object into a JSON object using the mapper provided.  
+	 * 
+	 * @param mapper 
+	 * @param object to convert
+	 * @return Object in JSON format stringified
+	 */
 	private static String toJSON(ObjectMapper mapper, Object object) {
 		try {
 			return mapper.writeValueAsString(object);
